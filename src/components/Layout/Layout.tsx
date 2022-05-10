f48c9895808bc63e5b140436fe6ea9717326bddf
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-10 21:50:08 +0900

    layoutの追加

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Navbar from '../Navbar/Navbar';
+
 const Layout:React.FC = ({ children }) => {
   
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <main>{ children }</main>
     </>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
-import { theme } from "../chakra/theme";
 import '../styles/globals.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { theme } from '../chakra/theme';
+import Layout from '../components/Layout/Layout';
+
+import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component  {...pageProps} />
-    </ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
   )
 }
 
