cda095a1444054eaae0dc8ed2a37f9e2b645e50a
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-10 23:02:08 +0900

    ヘッダー修正

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,33 @@
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import SearchInput from './SearchInput';
 
-const Navbar:React.FC = () => {
-  
-  return <div>Navbar</div>
+const Navbar: React.FC = () => {
+  return (
+    <Flex
+      bg='white'
+      height='44px'
+      padding='6px 12px'
+      justifyContent={{ md: 'space-between' }}
+    >
+      <Flex
+        align='center'
+        width={{ base: '40px', md: 'auto' }}
+        mr={{ base: 0, md: 2 }}
+        cursor='pointer'
+      >
+        <Image src='/images/redditFace.svg' height='30px' alt='' />
+        <Image
+          display={{ base: 'none', md: 'unset' }}
+          src='/images/redditText.svg'
+          height='46px'
+          alt=''
+        />
+      </Flex>
+      {/* {user && <Directory />} */}
+      <SearchInput />
+      {/* <RightContent user={user as User} /> */}
+    </Flex>
+  )
 }
-export default Navbar;
+export default Navbar
