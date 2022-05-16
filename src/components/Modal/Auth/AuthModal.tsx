f25ed24146216ef04b436e35f417c2be4e88d8cb
TARO <shun.ichiro.tanaka16@gmail.com>
2022-05-16 23:51:21 +0900

    修正

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,28 +1,37 @@
 import {
   Flex,
-  Modal,
   ModalBody,
   ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay
-} from '@chakra-ui/react'
-import React from 'react'
-import { useRecoilState } from 'recoil'
-import { authModalState } from '../../../atoms/authModalAtom'
-import AuthInputs from './AuthInputs'
-import OAuthButtons from './OAuthButtons'
-import ResetPassword from './ResetPassword'
+  ModalHeader
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
+import { userState } from "../../../atoms/userAtom";
+import { auth } from "../../../firebase/clientApp";
+import ModalWrapper from "../ModalWrapper";
+import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButtons";
+import ResetPassword from "./ResetPassword";
 
-const AuthModal: React.FC = () => {
-  const [modalState, setModalState] = useRecoilState(authModalState)
+type AuthModalProps = {};
 
+const AuthModal: React.FC<AuthModalProps> = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
   const handleClose = () =>
-    setModalState(prev => ({
+    setModalState((prev) => ({
       ...prev,
-      open: false
-    }))
+      open: false,
+    }));
 
+  const currentUser = useRecoilValue(userState);
+  const [user, error] = useAuthState(auth);
+
+  // Can implement at the end
+  // useEffect(() => {
+  //   if (currentUser) handleClose();
+  // }, [currentUser]);
   const toggleView = (view: string) => {
     setModalState({
       ...modalState,
@@ -30,46 +39,62 @@ const AuthModal: React.FC = () => {
     });
   };
 
+  useEffect(() => {
+    if (user) handleClose();
+  }, [user]);
+
   return (
-    <Modal isOpen={modalState.open} onClose={handleClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader
-            display='flex'
-            flexDirection='column'
-            alignItems='center'
-          >
-            {modalState.view === 'login' && 'Login'}
-            {modalState.view === 'signup' && 'Sign Up'}
-            {modalState.view === 'resetPassword' && 'Reset Password'}
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody
-            display='flex'
-            flexDirection='column'
-            alignItems='center'
-            justifyContent='center'
-            pb={6}
-          >
-            <Flex
-              direction='column'
-              alignItems='center'
-              justifyContent='center'
-              width='70%'
-            >
-             {modalState.view === "login" || modalState.view === "signup" ? (
+    <ModalWrapper isOpen={modalState.open} onClose={handleClose}>
+      <ModalHeader display="flex" flexDirection="column" alignItems="center">
+        {modalState.view === "login" && "Login"}
+        {modalState.view === "signup" && "Sign Up"}
+        {modalState.view === "resetPassword" && "Reset Password"}
+      </ModalHeader>
+      <ModalCloseButton />
+      <ModalBody
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        pb={6}
+      >
+        <Flex
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          width="70%"
+        >
+          {modalState.view === "login" || modalState.view === "signup" ? (
+            <>
+              <OAuthButtons />
+              OR
+              <AuthInputs toggleView={toggleView} />
+            </>
+          ) : (
+            <ResetPassword toggleView={toggleView} />
+          )}
+          {/* // Will implement at end of tutorial */}
+          {/* {user && !currentUser && (
                 <>
-                  <OAuthButtons />
-                  OR
-                  <AuthInputs toggleView={toggleView} />
+                  <Spinner size="lg" mt={2} mb={2} />
+                  <Text fontSize="8pt" textAlign="center" color="blue.500">
+                    You are logged in. You will be redirected soon
+                  </Text>
                 </>
+              )} */}
+          {/* {false ? (
+                <Flex
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  height="100%"
+                >
+                </Flex>
               ) : (
-                <ResetPassword toggleView={toggleView} />
-              )}
-            </Flex>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-  )
-}
-export default AuthModal
+              )} */}
+        </Flex>
+      </ModalBody>
+    </ModalWrapper>
+  );
+};
+export default AuthModal;
--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -3,19 +3,14 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
 
-type AuthButtonsProps = {};
-
-// ----------------------
-// NOTE 認証モーダルのボタン
-// ----------------------
-const AuthButtons: React.FC<AuthButtonsProps> = () => {
+const AuthButtons: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
 
   return (
     <>
       <Button
         variant="outline"
-        height="40px"
+        height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
@@ -25,7 +20,7 @@ const AuthButtons: React.FC<AuthButtonsProps> = () => {
       </Button>
       <Button
         variant="solid"
-        height="40px"
+        height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
         mr={2}
